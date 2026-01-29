d5cf517a73d49c3dabd0be294509a0a84acf137c
Chuy-pro <xdchuy04@gmail.com>
2026-01-29 12:26:00 -0600

    Add index.php file with team introduction and member links

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>BIENVENIDOS!!!</h1>
+    <br>
+    <h2>Equipo Ecoskin</h2>
+    <br>
+    <h2>Integrantes:</h2>
+    <ul>
+        <li><a href="./jesus.docx">Jesus Alejandro Perez Rodriguez</a></li>
+        <li><a href="./obet.docx">Obet Lopez Lopez</a></li>
+        <li><a href="./marbel.docx">Marbel Dayaña Peñate Lopez</a></li>
+        <li><a href="./orquidea.docx">Orquidea Alondra Rodriguez Solozarno</a></li>
+        <li><a href="./karen.docx">Karen Christal Rodriguez Mendez</a></li>
+
+</body>
+</html>
